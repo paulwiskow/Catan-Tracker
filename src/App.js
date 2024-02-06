ec6a4c7bf0356b1ec89494a9092f71330cc06835
Paul Wiskow <usk7dg@virginia.edu>
2024-02-06 11:06:29 -0500

    Fixed problems with the player resource chart and added the appropriate colors to the bars.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import Build from "./components/Build";
 function App() {
   const [players, setPlayers] = react.useState(() => createPlayerArray());
   const [toggle, setToggle] = react.useState(true);
+  console.log(players)
 
   const style_main = toggle ? { backgroundColor: "#6f746f8f" } : {};
   const style_graph = !toggle ? { backgroundColor: "#6f746f8f" } : {};
@@ -107,7 +108,6 @@ function App() {
   }
 
   const playerComponents = players.map((player) => {
-    // console.log("success")
     const index = player.playerNum - 1;
     const buildingComponents = player.buildings.map((building) => {
       return (
@@ -137,17 +137,18 @@ function App() {
   const dieData = formDiceData();
   const playerResourceData = formPlayerData() // formatted as [wood, brick, sheep, wheat, ore]
   const playerNames = players.map((player) => player.name)
+  console.log(playerResourceData[0])
 
   function formPlayerData() {
 	let temp = [[], [], [], [], []]
 
 	for(const element of players) {
 		for(let j = 0; j < 5; j++) {
-			temp[j].push({ player: element.num, resource: element.resourcesGained[j] })
+			temp[j].push({ player: element.playerNum, resource: element.resourcesGained[j] })
 		}
 	}
 
-	console.log(temp)
+    console.log(temp)
 
 	return temp
   }
@@ -183,40 +184,39 @@ function App() {
 
   function trackResources(dieNum, increment) {
     for (const player of players) {
-      for (const building of player.buildings) {
-        if (building === null) {
-          break;
-        } else {
-          for (let tile in building.tiles) {
-            if (tile.diceRoll === dieNum) {
-              if (building.isCity) {
-                if (increment) {
-                  player.resourcesGained[
-                    convertResourceToIndex(tile.resource)
-                  ] += 2;
-                } else {
-                  player.resourcesGained[
-                    convertResourceToIndex(tile.resource)
-                  ] -= 2;
+        const temp = Array.from(player.resourcesGained)
+        for (const building of player.buildings) {
+            if (building === null) {
+            break;
+            } else {
+                for (const tile of building.tiles) {
+                    if (tile.diceRoll === dieNum) {
+                        if (building.isCity) {
+                            if (increment) {
+                            temp[
+                                convertResourceToIndex(tile.resource)
+                            ] += 2;
+                            } else {
+                            temp[
+                                convertResourceToIndex(tile.resource)
+                            ] -= 2;
+                            }
+                        } else {
+                            if (increment) {
+                            temp[
+                                convertResourceToIndex(tile.resource)
+                            ] += 1;
+                            } else {
+                            temp[
+                                convertResourceToIndex(tile.resource)
+                            ] -= 1;
+                            }
+                        }
+                        updateResourcesGain(player.playerNum, temp) // create different function for updating all this IMPORTANT ***********
+                    }
                 }
-              } else {
-                if (increment) {
-                  player.resourcesGained[
-                    convertResourceToIndex(tile.resource)
-                  ] += 1;
-                } else {
-                  player.resourcesGained[
-                    convertResourceToIndex(tile.resource)
-                  ] -= 1;
-                }
-              }
-
-			  console.log(player)
-              updateResourcesGain(player.num, player.resourcesGained) // create different function for updating all this IMPORTANT ***********
             }
-          }
         }
-      }
     }
   }
 
@@ -296,7 +296,7 @@ function App() {
 				</VictoryChart>
 			</div>
 			<div className="player-resource-chart-container">
-				<VictoryChart>
+				<VictoryChart domainPadding={30}>
 					<VictoryAxis 
 						tickValues={[1, 2, 3, 4]} 
 						tickFormat={playerNames} 
@@ -316,13 +316,57 @@ function App() {
 						}}
 					/>
 					<VictoryStack>
-						<VictoryBar data={playerResourceData[0]} x="Player" y="Resources Gained" />
-						<VictoryBar data={playerResourceData[1]} x="Player" y="Resources Gained" />
-						<VictoryBar data={playerResourceData[2]} x="Player" y="Resources Gained" />
-						<VictoryBar data={playerResourceData[3]} x="Player" y="Resources Gained" />
-						<VictoryBar data={playerResourceData[4]} x="Player" y="Resources Gained" />
+					    <VictoryBar 
+                            data={playerResourceData[0]} 
+                            x="player" 
+                            y="resource" 
+                            style={{
+                                data: {
+                                    fill: "#3ecd21"
+                                }
+                            }}
+                        />
+						<VictoryBar 
+                            data={playerResourceData[1]} 
+                            x="player" 
+                            y="resource" 
+                            style={{
+                                data: {
+                                    fill: "#ce866d"
+                                }
+                            }}
+                        />
+                        <VictoryBar 
+                            data={playerResourceData[2]} 
+                            x="player" 
+                            y="resource" 
+                            style={{
+                                data: {
+                                    fill: "#9fe21b"
+                                }
+                            }}
+                        />
+                        <VictoryBar 
+                            data={playerResourceData[3]} 
+                            x="player" 
+                            y="resource" 
+                            style={{
+                                data: {
+                                    fill: "#e8b339"
+                                }
+                            }}
+                        />
+                        <VictoryBar 
+                            data={playerResourceData[4]} 
+                            x="player" 
+                            y="resource" 
+                            style={{
+                                data: {
+                                    fill: "#a2c9bc"
+                                }
+                            }}
+                        />
 					</VictoryStack>
-					
 				</VictoryChart>
 			</div>
 		</div>}
