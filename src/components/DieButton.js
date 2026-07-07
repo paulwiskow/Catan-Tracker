506828fd4518269d464c79fb914dfb7ded607fd0
Paul Wiskow <paulwiskow@gmail.com>
2026-07-07 17:37:43 -0400

    Started angular refactor

--- src/components/DieButton.js
+++
@@ -1,9 +0,0 @@
-import '../style/dice.css'
-
-export default function DieButton(props) {
-    return (
-        <div className="die-button-container">
-            <button className="die-button" onClick={() => props.increase(props.value, true)}>{props.value} ({props.track})</button>
-        </div>
-    )
-}
