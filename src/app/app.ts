6320286a41ff5e28aa3ce5b497a526f01707b723
Paul Wiskow <paulwiskow@gmail.com>
2026-07-13 14:36:55 -0400

    Able to display full board now

--- src/app/app.ts
+++ src/app/app.ts
@@ -48,18 +48,64 @@ export class App implements OnDestroy {
     }
 
     draw_board(width: number, height: number): void {
-        let temp_hex: hexagon = {
-            center_x: width / 2,
-            center_y: height / 2,
-            radius: 100,
-            type: "wood",
-            num: -1,
+        let center_x: number = width / 2;
+        let center_y: number = height / 2;
+        let radius: number = Math.min(width, height) / 10;
+
+        // Angle 0 starts at same place, but goes clockwise instead of counter clockwise
+        // outer ring is 3 radii from center when angle % 60 != 0 
+        // Otherwise, it's 4 * (radi / 2) * sqrt(3) = 2 * radi * sqrt(3)
+        // next ring is same thing but only the 60 degree increments
+        let outer_starting_angle: number = 0; // in degrees
+        let dist_30: number = 3 * radius;
+        let dist_60: number = 2 * radius * Math.sqrt(3);
+        for (let i = 0; i < 12; i++) {
+            let radians: number = outer_starting_angle * (Math.PI / 180);
+
+            let temp_hex: hexagon = {
+                center_x: outer_starting_angle % 60 == 0 ? center_x + dist_60 * Math.cos(radians) : center_x + dist_30 * Math.cos(radians),
+                center_y: outer_starting_angle % 60 == 0 ? center_y + dist_60 * Math.sin(radians) : center_y + dist_30 * Math.sin(radians),
+                radius: radius,
+                type: i == 0 ? "wood" : "ore",
+                num: radius,
+            }
+
+            this.draw_hexagon(temp_hex);
+
+            outer_starting_angle -= 30;
+            outer_starting_angle = outer_starting_angle % 360;
         }
 
-        this.draw_hexagon(temp_hex);
+
+        outer_starting_angle = 0;
+        dist_60 = dist_60 / 2;
+        for (let i = 0; i < 6; i++) {
+            let radians: number = outer_starting_angle * (Math.PI / 180);
+            let temp_hex: hexagon = {
+                center_x: center_x + dist_60 * Math.cos(radians),
+                center_y: center_y + dist_60 * Math.sin(radians),
+                radius: radius,
+                type: i == 0 ? "brick" : "wheat",
+                num: -1,
+            }
+            this.draw_hexagon(temp_hex);
+
+            outer_starting_angle -= 60;
+            outer_starting_angle = outer_starting_angle % 360;
+        }
+
+        this.draw_hexagon({
+            center_x: center_x,
+            center_y: center_y,
+            radius: radius,
+            type: "sheep",
+            num: 0
+        });
     }
 
     draw_hexagon(hexagon: hexagon): void {
+        this.ctx.lineWidth = hexagon['radius'] / 25;
+        this.ctx.strokeStyle = 'black';
         this.ctx.beginPath();
         this.ctx.moveTo(hexagon['center_x'], hexagon['center_y'] - hexagon['radius']);
 
@@ -100,6 +146,7 @@ export class App implements OnDestroy {
 
         this.ctx.fillStyle = color;
         this.ctx.fill();
+        this.ctx.stroke();
         this.ctx.closePath();
 
 
@@ -117,7 +164,7 @@ export class App implements OnDestroy {
             this.ctx.font = `bold ${hexagon['radius'] / 6}px Arial`;
             this.ctx.textAlign = 'center';
             this.ctx.textBaseline = 'middle';
-            this.ctx.fillText("-1", hexagon['center_x'], hexagon['center_y']);
+            this.ctx.fillText(`${hexagon['num']}`, hexagon['center_x'], hexagon['center_y']);
         }
     }
 
